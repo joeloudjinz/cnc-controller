e7e0e735b25c3ce1e75d67697a9391b21de48946
abdellah addoun <addoun.abdellah@gmail.com>
2019-06-06 15:18:09 +0100

    Opening dayabase connection in database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,26 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv').config();
 
-let connection = undefined;
+const credentials = {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+};
+
+let connection = mysql.createConnection(credentials);
 
 module.exports = {
-    initializeConnection: (credentials) => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                connection = await mysql.createConnection(credentials);
-                connection != undefined ? resolve(true) : resolve(false);
-            } catch (error) {
-                reject(error.message);
-            }
-        });
-    },
+    // initializeConnection: (credentials) => {
+    //     return new Promise(async (resolve, reject) => {
+    //         try {
+    //             connection = await mysql.createConnection(credentials);
+    //             connection != undefined ? resolve(true) : resolve(false);
+    //         } catch (error) {
+    //             reject(error.message);
+    //         }
+    //     });
+    // },
     openConnection: () => {
         connection.connect((error) => {
             if (error) {
