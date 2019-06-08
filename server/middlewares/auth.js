9b36c9d3dd497468c5123fb6fa086998ec8bc8a9
abdellah addoun <addoun.abdellah@gmail.com>
2019-06-08 18:31:33 +0100

    Updating public folder

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -5,28 +5,34 @@ const controller = require(authControllerPath);
 module.exports = async (req, res, next) => {
     if (req.headers.authorization) {
         const oldToken = req.headers.authorization.split(' ')[1] || req.body.token || req.params.token;
-        // console.log('oldToken :', oldToken);
-        controller.validateToken(oldToken)
-            .then((result) => {
-                next();
-            }).catch((error) => {
-                if (error.name === 'TokenExpiredError') {
-                    res.status(406).send({
-                        failure: 'Not Acceptable, access token has expired',
-                        error
-                    });
-                } else {
-                    return res.status(401).send({
-                        failure: 'Unauthorized, invalid access token!',
-                        error
-                    });
-                }
+        console.log('oldToken :', oldToken);
+        if (oldToken) {
+            controller.validateToken(oldToken)
+                .then((result) => {
+                    next();
+                }).catch((error) => {
+                    if (error.name === 'TokenExpiredError') {
+                        return res.status(406).send({
+                            failure: 'Not Acceptable, access token has expired',
+                            error
+                        });
+                    } else {
+                        return res.status(401).send({
+                            failure: 'Unauthorized, invalid access token!',
+                            error
+                        });
+                    }
+                });
+        } else {
+            return res.status(400).send({
+                failure: 'Bad Request, No access token!',
             });
-    }else{
+        }
+    } else {
         //? 400 => Bad Request
         return res.status(400).send({
             failure: 'Bad Request, No access token!',
         });
     }
-    
+
 }
